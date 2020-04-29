cd8746134cf1f4d1dc6f2275cba07bbb0c45f2f9
Rodrigo <rcuenca446@gmail.com>
2020-04-29 15:02:33 -0500

    Updates

---
+++ precache-manifest.0893e770dd87b2d73122943d34f8a1d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8e2fa960725164a4b4ee5a22a9efb4",
+    "url": "/Port/index.html"
+  },
+  {
+    "revision": "f1053b489691599c1896",
+    "url": "/Port/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "41f9a948915a33a0dd3a",
+    "url": "/Port/static/css/main.2b366012.chunk.css"
+  },
+  {
+    "revision": "f1053b489691599c1896",
+    "url": "/Port/static/js/2.66365743.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Port/static/js/2.66365743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f9a948915a33a0dd3a",
+    "url": "/Port/static/js/main.3fda0654.chunk.js"
+  },
+  {
+    "revision": "e4ffb0cb69d548b84f63",
+    "url": "/Port/static/js/runtime-main.a3e29ac1.js"
+  }
+]);
